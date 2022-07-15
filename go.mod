8439374a5052ea57ad8fd609c7583f45b0ca3717
Shane Clark <ff.shane.clark@outlook.com>
2022-07-14 21:02:28 -0600

    Add code to get and link pak files.

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require github.com/urfave/cli/v2 v2.11.0
 
 require (
 	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
+	github.com/nmrshll/go-cp v0.0.0-20180115193924-61436d3b7cfa
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
 )
--- starbound-workshop-linker.go
+++ starbound-workshop-linker.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"fmt"
+	"io/fs"
 	"log"
 	"os"
+	"path/filepath"
 
+	"github.com/nmrshll/go-cp"
 	"github.com/urfave/cli/v2"
 )
 
@@ -31,4 +35,44 @@ func main() {
 	if err := App.Run(os.Args); err != nil {
 		log.Fatalf("Failed to start application: %v", err)
 	}
+}
+
+func getPaks(dir string) ([]Mod, error) {
+	returnString := []Mod{}
+	if err := filepath.WalkDir(dir, func(path string, d fs.DirEntry, err error) error {
+		if d.IsDir() || filepath.Ext(path) != ".pak" {
+			return nil
+		}
+
+		returnString = append(returnString, Mod{ID: filepath.Base(filepath.Dir(path)), Path: path})
+		return nil
+	}); err != nil {
+		return nil, err
+	}
+
+	return returnString, nil
+}
+
+func linkPaks(dir string, paks []Mod) ([]string, error) {
+	returnString := []string{}
+	for _, pak := range paks {
+		newPath := fmt.Sprintf("%s%c%s.pak", dir, os.PathSeparator, pak.ID)
+		if err := os.Symlink(pak.Path, newPath); err != nil {
+			return nil, err
+		}
+		returnString = append(returnString, newPath)
+	}
+
+	return returnString, nil
+}
+
+func copyPaks(dir string, paks []Mod) ([]string, error) {
+	returnString := []string{}
+	for _, pak := range paks {
+		newPath := fmt.Sprintf("%s%c%s.pak", dir, os.PathSeparator, pak.ID)
+		cp.CopyFile(pak.Path, newPath)
+		returnString = append(returnString, newPath)
+	}
+
+	return returnString, nil
 }
